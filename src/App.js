54b49095529ef25c60ec86228c7b3f7c215b1c07
shivprajapat <shivkumarprajapat232@gmail.com>
2022-03-22 11:42:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import './sass/App.scss'
 import Home from './components/Right/friend/Home'
-import Easier from './components/Right/easier/Easier'
 
 const App = () => {
   return (
     <main>
-      <Easier />
       <div id="left">
       </div>
       <div id="right">
-        {/* <Home /> */}
+        <Home />
       </div>
     </main>
   )
